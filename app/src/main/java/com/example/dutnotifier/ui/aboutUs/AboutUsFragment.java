b16c71fd50090307c920f310b35eaa3fe935bbc3
ducminhle99 <ducminhle231@gmail.com>
2019-11-27 19:50:39 +0700

    chỉnh sửa giao diện và thêm vài chức năng

--- app/src/main/java/com/example/dutnotifier/ui/aboutUs/AboutUsFragment.java
+++ app/src/main/java/com/example/dutnotifier/ui/aboutUs/AboutUsFragment.java
@@ -1,26 +1,22 @@
-package com.example.dutnotifier.ui.dashboard;
+package com.example.dutnotifier.ui.aboutUs;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dutnotifier.R;
 
-public class DashboardFragment extends Fragment {
+public class AboutUsFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_aboutus, container, false);
   //      final TextView textView = root.findViewById(R.id.text_dashboard);
         return root;
     }
--- app/src/main/java/com/example/dutnotifier/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/dutnotifier/ui/search/SearchFragment.java
@@ -1,4 +1,4 @@
-package com.example.dutnotifier.ui.notifications;
+package com.example.dutnotifier.ui.search;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,25 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dutnotifier.R;
 
-public class NotificationsFragment extends Fragment {
+public class SearchFragment extends Fragment {
     private EditText mEdtInput;
     private Button mBtnSearch;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
         mEdtInput = (EditText) root.findViewById(R.id.edt_input);
         mBtnSearch = (Button) root.findViewById(R.id.btn_search);
         return root;
